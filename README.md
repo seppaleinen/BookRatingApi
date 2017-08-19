e92f9a779f750ef3ec8eb07e62091e93ef2b44ce
David Eriksson <davideriksson@swedenmail.com>
2017-08-19 11:07:21 +0200

    Added simple api

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# BookRatingApi
+# Book Rating API
+
+
+### Commands
+
+```bash
+go build src/*.go -o bookrating
+
+go test src/*.go
+```
---
+++ src/repo.go
@@ -0,0 +1,30 @@
+package main
+
+
+var currentId int
+
+var todos Todos
+
+// Give us some seed data
+func init() {
+	RepoCreateTodo(Todo{Name: "Write presentation"})
+	RepoCreateTodo(Todo{Name: "Host meetup"})
+}
+
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.Id == id {
+			return t
+		}
+	}
+	// return empty Todo if not found
+	return Todo{}
+}
+
+//this is bad, I don't think it passes race condtions
+func RepoCreateTodo(t Todo) Todo {
+	currentId += 1
+	t.Id = currentId
+	todos = append(todos, t)
+	return t
+}
